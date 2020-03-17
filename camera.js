5e58e607b24e6adeb8934fc5b9ff500fe2a674b1
ivyxuan <iloveivyxuan@gmail.com>
2020-03-17 11:32:28 -0500

    Change component to hook

--- camera.js
+++ camera.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Camera } from 'expo-camera';
-import * as Permissions from 'expo-permissions';
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
 import { Text, View, TouchableOpacity } from "react-native";
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const CameraDemo = () => {
   const [permission, setPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef(null);
 
   useEffect(() => {
     const askPermissions = async () => {
@@ -21,19 +25,63 @@ const CameraDemo = () => {
     askPermissions();
   }, []);
 
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images
+    });
+  };
+
+  const switchCamera = () => {
+    setCameraType(
+      cameraType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const { uri } = await cameraRef.current.takePictureAsync();
+      MediaLibrary.saveToLibraryAsync(uri);
+    }
+  };
+
   if (permission === null) {
     return <View />;
   } else if (permission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>No access to camera and/or camera roll</Text>;
   } else {
+    const buttonStyle = {
+      alignSelf: "flex-end",
+      alignItems: "center",
+      backgroundColor: "transparent"
+    };
+    const iconStyle = { color: "#fff", fontSize: 40 };
     return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+      <Camera style={{ flex: 1 }} type={cameraType} ref={cameraRef}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            margin: 20
+          }}
+        >
+          <TouchableOpacity style={buttonStyle} onPress={pickImage}>
+            <Ionicons name="ios-photos" style={iconStyle} />
+          </TouchableOpacity>
+          <TouchableOpacity style={buttonStyle} onPress={takePicture}>
+            <FontAwesome name="camera" style={iconStyle} />
+          </TouchableOpacity>
+          <TouchableOpacity style={buttonStyle} onPress={switchCamera}>
+            <MaterialCommunityIcons name="camera-switch" style={iconStyle} />
+          </TouchableOpacity>
+        </View>
+      </Camera>
 
-          </Camera>
-      </View>
     );
   }
 }
 
-export default CameraDemo;
+
+export default CameraDemo
